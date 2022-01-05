dc9334c310631b55bf865c8fbff43285026e1ab8
SwiftSurgeon <catowner11@yahoo.com>
2022-01-05 10:28:35 -0600

    added Color Library
    introduced multiple themes as per https://www.envato.com/blog/color-scheme-trends-in-mobile-app-design/

--- SurgeUI/Sources/SurgeUI/Atoms/Colors/Color.swift
+++ SurgeUI/Sources/SurgeUI/Atoms/Colors/Color.swift
@@ -1,6 +1,6 @@
 //
 //  Color.swift
-//
+//  See: https://www.envato.com/blog/color-scheme-trends-in-mobile-app-design/
 
 import SwiftUI
 
@@ -8,7 +8,42 @@ import SwiftUI
 public extension SurgeUI {
     
     struct Color {
-        // Palettes
+        // Minimal Muted Color Palette
+        public static let MMC_Lavender = SwiftUI.Color("MMC_Lavender", bundle: .module)
+        public static let MMC_Gray = SwiftUI.Color("MMC_Gray", bundle: .module)
+        public static let MMC_LightBrown = SwiftUI.Color("MMC_LightBrown", bundle: .module)
+        public static let MMC_LightGray = SwiftUI.Color("MMC_LightGray", bundle: .module)
+        public static let MMC_LightLavender = SwiftUI.Color("MMC_LightLavender", bundle: .module)
+        // Bright Vibrant Color Palette
+        public static let BV_GrayPink = SwiftUI.Color("BV_GrayPink", bundle: .module)
+        public static let BV_Pink = SwiftUI.Color("BV_Pink", bundle: .module)
+        public static let BV_Gray = SwiftUI.Color("BV_Gray", bundle: .module)
+        public static let BV_DarkGray = SwiftUI.Color("BV_DarkGray", bundle: .module)
+        public static let BV_LightGray = SwiftUI.Color("BV_LightGray", bundle: .module)
+        // Colorful Icons and Accent Color Palette
+        public static let CIA_LightPurple = SwiftUI.Color("CIA_LightPurple", bundle: .module)
+        public static let CIA_LightPink = SwiftUI.Color("CIA_LightPink", bundle: .module)
+        public static let CIA_LightGreen = SwiftUI.Color("CIA_LightGreen", bundle: .module)
+        public static let CIA_White = SwiftUI.Color("CIA_White", bundle: .module)
+        public static let CIA_BurntOrange = SwiftUI.Color("CIA_BurntOrange", bundle: .module)
+        // Minimalist Monochrome Color Palette
+        public static let MM_Gray = SwiftUI.Color("MM_Gray", bundle: .module)
+        public static let MM_LightGray = SwiftUI.Color("MM_LightGray", bundle: .module)
+        public static let MM_White = SwiftUI.Color("MM_White", bundle: .module)
+        public static let MM_MidnightGray = SwiftUI.Color("MM_MidnightGray", bundle: .module)
+        public static let MM_DarkGray = SwiftUI.Color("MM_DarkGray", bundle: .module)
+        // Dreamy Gradients Color Palette
+        public static let DG_White = SwiftUI.Color("DG_White", bundle: .module)
+        public static let DG_Blue = SwiftUI.Color("DG_Blue", bundle: .module)
+        public static let DG_Purple = SwiftUI.Color("DG_Purple", bundle: .module)
+        public static let DG_Pink = SwiftUI.Color("DG_Pink", bundle: .module)
+        public static let DG_Lavendar = SwiftUI.Color("DG_Lavendar", bundle: .module)
+        // Colorful Flat IllustrationsColor Palette
+        public static let CFI_Blue = SwiftUI.Color("CFI_Blue", bundle: .module)
+        public static let CFI_White = SwiftUI.Color("CFI_White", bundle: .module)
+        public static let CFI_Yellow = SwiftUI.Color("CFI_Yellow", bundle: .module)
+        public static let CFI_Orange = SwiftUI.Color("CFI_Orange", bundle: .module)
+        public static let CFI_Green = SwiftUI.Color("CFI_Green", bundle: .module)
     }
     
 }
--- SurgeUIDemo/SurgeUIDemo/Scenarios/Atoms/ColorView.swift
+++ SurgeUIDemo/SurgeUIDemo/Scenarios/Atoms/ColorView.swift
@@ -7,10 +7,208 @@ import SurgeUI
 
 public struct ColorView: View {
 
+    let size: CGFloat = 45.0
+    
     public var body: some View {
         NavigationView {
             List {
-                // list colors here TBD
+                Section(header: Text("Minimal Muted Color Palette")) {
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MMC_LightLavender)
+                            .frame(width: size, height: size)
+                        Text("MMC_LightLavender")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MMC_Gray)
+                            .frame(width: size, height: size)
+                        Text("MMC_Gray")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MMC_LightGray)
+                            .frame(width: size, height: size)
+                        Text("MMC_LightGray")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MMC_LightBrown)
+                            .frame(width: size, height: size)
+                        Text("MMC_LightBrown")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MMC_Lavender)
+                            .frame(width: size, height: size)
+                        Text("MMC_Lavender")
+                    }
+                }
+                
+                Section(header: Text("Bright Vibrant Color Palette")) {
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.BV_GrayPink)
+                            .frame(width: size, height: size)
+                        Text("BV_GrayPink")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.BV_Pink)
+                            .frame(width: size, height: size)
+                        Text("BV_Pink")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.BV_Gray)
+                            .frame(width: size, height: size)
+                        Text("BV_Gray")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.BV_DarkGray)
+                            .frame(width: size, height: size)
+                        Text("BV_DarkGray")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.BV_LightGray)
+                            .frame(width: size, height: size)
+                        Text("BV_LightGray")
+                    }
+                }
+                
+                Section(header: Text("Colorful Icons and Accent Color Palette")) {
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CIA_LightPurple)
+                            .frame(width: size, height: size)
+                        Text("CIA_LightPurple")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CIA_LightPink)
+                            .frame(width: size, height: size)
+                        Text("CIA_LightPink")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CIA_LightGreen)
+                            .frame(width: size, height: size)
+                        Text("CIA_LightGreen")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CIA_White)
+                            .frame(width: size, height: size)
+                        Text("CIA_White")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CIA_BurntOrange)
+                            .frame(width: size, height: size)
+                        Text("CIA_BurntOrange")
+                    }
+                }
+                
+                Section(header: Text("Minimalist Monochrome Color Palette")) {
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MM_Gray)
+                            .frame(width: size, height: size)
+                        Text("MM_Gray")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MM_LightGray)
+                            .frame(width: size, height: size)
+                        Text("MM_LightGray")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MM_White)
+                            .frame(width: size, height: size)
+                        Text("MM_White")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MM_MidnightGray)
+                            .frame(width: size, height: size)
+                        Text("MM_MidnightGray")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.MM_DarkGray)
+                            .frame(width: size, height: size)
+                        Text("MM_DarkGray")
+                    }
+                }
+                
+                Section(header: Text("Dreamy Gradients Color Palette")) {
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.DG_White)
+                            .frame(width: size, height: size)
+                        Text("DG_White")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.DG_Blue)
+                            .frame(width: size, height: size)
+                        Text("DG_Blue")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.DG_Purple)
+                            .frame(width: size, height: size)
+                        Text("DG_Purple")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.DG_Pink)
+                            .frame(width: size, height: size)
+                        Text("DG_Pink")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.DG_Lavendar)
+                            .frame(width: size, height: size)
+                        Text("DG_Lavendar")
+                    }
+                }
+                
+                Section(header: Text("Colorful Flat Illustrations Color Palette")) {
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CFI_Blue)
+                            .frame(width: size, height: size)
+                        Text("CFI_Blue")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CFI_White)
+                            .frame(width: size, height: size)
+                        Text("CFI_White")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CFI_Yellow)
+                            .frame(width: size, height: size)
+                        Text("CFI_Yellow")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CFI_Orange)
+                            .frame(width: size, height: size)
+                        Text("CFI_Orange")
+                    }
+                    HStack {
+                        Rectangle()
+                            .fill(SurgeUI.Color.CFI_Green)
+                            .frame(width: size, height: size)
+                        Text("CFI_Green")
+                    }
+                }
             }
             .navigationBarTitle(Text("Colors"))
         }
