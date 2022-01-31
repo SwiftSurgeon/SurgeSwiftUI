cffa78e1832df4f329eab99813f4d41f8a1f66f2
Carlo <catowner11@yahoo.com>
2022-01-31 00:32:12 -0600

    Add more free Fonts to SurgeUI package, add more examples to SwiftUI Demo
    Add more free Fonts to SurgeUI package, add more examples to SwiftUI Demo

--- SurgeUI/Sources/SurgeUI/Atoms/Fonts/Font+Typography.swift
+++ SurgeUI/Sources/SurgeUI/Atoms/Fonts/Font+Typography.swift
@@ -8,7 +8,17 @@ import SwiftUI
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension SurgeUI {
     public struct Font {
+        
+        public static let IMP_Block: SwiftUI.Font = SwiftUI.Font.custom("Impressions-Regular", size: 16.0)
+        
         public static let JFS_Regular: SwiftUI.Font = SwiftUI.Font.custom("JacquesFrancoisShadow-Regular", size: 16.0)
+        
+        public static let LS_Regular: SwiftUI.Font = SwiftUI.Font.custom("LondrinaShadow-Regular", size: 16.0)
+        
+        public static let VS_Regular: SwiftUI.Font = SwiftUI.Font.custom("VastShadow-Regular", size: 16.0)
+        
+        public static let WCW_Regular: SwiftUI.Font = SwiftUI.Font.custom("WCWunderbachPaintBta", size: 16.0)
+        
         
         public init(){
         }
--- SurgeUIDemo/SurgeUIDemo/Scenarios/Atoms/FontsView.swift
+++ SurgeUIDemo/SurgeUIDemo/Scenarios/Atoms/FontsView.swift
@@ -1,5 +1,5 @@
 //
-//  FontssView.swift
+//  FontsView.swift
 //
 
 import SwiftUI
@@ -10,6 +10,20 @@ public struct FontsView: View {
     public var body: some View {
         NavigationView {
             List {
+                Section(header: Text("Impressions Font - PSI_Block")) {
+                        VStack {
+                            Text("Do not go where the path may lead, go instead where there is no path and leave a trail.")
+                                .font(SurgeUI.Font.IMP_Block)
+                                .foregroundColor(SurgeUI.Color.MM_MidnightGray)
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                Text("Ralph Waldo Emerson")
+                                    .font(SurgeUI.Font.IMP_Block)
+                                    .foregroundColor(SurgeUI.Color.CFI_Blue)
+                            }
+                        }
+                }
                 Section(header: Text("Jacques Francois Shadow Font - JFS_Regular")) {
                     Group{
                         VStack {
@@ -21,6 +35,54 @@ public struct FontsView: View {
                                 Spacer()
                                 Text("Mark Twain")
                                     .font(SurgeUI.Font.JFS_Regular)
+                                    .foregroundColor(SurgeUI.Color.CFI_Blue)
+                            }
+                        }
+                    }
+                }
+                Section(header: Text("Londrina Shadow Font - LS_Regular")) {
+                    Group{
+                        VStack {
+                            Text("The greatest glory in living lies not in never falling, but in rising every time we fall.")
+                                .font(SurgeUI.Font.LS_Regular)
+                                .foregroundColor(SurgeUI.Color.MM_MidnightGray)
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                Text("Nelson Mandela")
+                                    .font(SurgeUI.Font.LS_Regular)
+                                    .foregroundColor(SurgeUI.Color.CFI_Blue)
+                            }
+                        }
+                    }
+                }
+                Section(header: Text("Vast Shadow Font - VS_Regular")) {
+                    Group{
+                        VStack {
+                            Text("Tell me and I forget. Teach me and I remember. Involve me and I learn.")
+                                .font(SurgeUI.Font.VS_Regular)
+                                .foregroundColor(SurgeUI.Color.MM_MidnightGray)
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                Text("Benjamin Franklin")
+                                    .font(SurgeUI.Font.VS_Regular)
+                                    .foregroundColor(SurgeUI.Color.CFI_Blue)
+                            }
+                        }
+                    }
+                }
+                Section(header: Text("WCWunderbach Paint Font - WCW_Regular")) {
+                    Group{
+                        VStack {
+                            Text("Don't judge each day by the harvest you reap but by the seeds that you plant.")
+                                .font(SurgeUI.Font.WCW_Regular)
+                                .foregroundColor(SurgeUI.Color.MM_MidnightGray)
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                Text("Robert Louis Stevenson")
+                                    .font(SurgeUI.Font.WCW_Regular)
                                     .foregroundColor(SurgeUI.Color.CFI_Blue)
                             }
                         }
